1a3af6aed939594b884d7f41fd3dff03e63f9e72
actsasrob <acts.as.rob@gmail.com>
2020-09-18 16:44:39 -0400

    Initial check-in

---
+++ ng-server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm use 12.18.4
+
+cd autom-dash 
+ng serve --port 4201 --open
