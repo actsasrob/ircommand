3af41bfb4bb5f6fb48b053c937f558bcf9c8e447
actsasrob <acts.as.rob@gmail.com>
2021-01-01 17:53:18 -0500

    Initial check-in

---
+++ cloud/run_express.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+logfile=/tmp/express.out
+
+export NVM_DIR="/home/aduser/.nvm"
+[ -s "/home/aduser/.nvm/nvm.sh" ] && . "/home/aduser/.nvm/nvm.sh"
+
+env >> $logfile
+cd /home/aduser/ircommand/autom-dash/server_orm 
+npm start >> $logfile 2>&1
